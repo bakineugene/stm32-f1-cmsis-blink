5f1c58441fbcb1d2905829940592f38f47fdfa1a
Eugene Bakin <bakin.eugene@ya.ru>
2025-11-06 12:18:10 +0500

    Adds actual blinking

--- src/main.c
+++ src/main.c
@@ -3,5 +3,19 @@
 // Need that for some reason
 void _init(){}
 
-int main(void) {
+void main(void)
+{
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);
+    GPIOC->CRH |= GPIO_CRH_MODE13_1;
+
+    while (1)
+    {
+        GPIOC->ODR |= GPIO_ODR_ODR13;
+        for (int i = 0; i < 500000; i++)
+            ; // arbitrary delay
+        GPIOC->ODR &= ~GPIO_ODR_ODR13;
+        for (int i = 0; i < 100000; i++)
+            ; // arbitrary delay
+    }
 }
